8a5861c399c5b6728282bf6f55d1c94903d19e79
solitude233-ui <64570960+solitude233-ui@users.noreply.github.com>
2021-05-05 19:35:18 -0400

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -3,8 +3,8 @@
 	<div id="site-title">Speltacular Baking</div>
     <nav>
     	<ul>
-    		<li><a href="/~14zxg/a5/index.php">Home</a></li>
-    		<li><a href="/~14zxg/a5/recipes.php">Recipes</a></li>
+    		<li><a href="./index.php">Home</a></li>
+    		<li><a href="./recipes.php">Recipes</a></li>
     	</ul>
     </nav>
 </header>
